e9429225ac5d2fb7d5f7d52ec94260d785f75b6f
mina <yegrinaaa@gmail.com>
2021-01-12 16:59:46 +0900

    0112 | Mat | 배너작업시작

---
+++ front-end/offertunity-app/src/pages/banner/Banner.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Banner = () => {
+  return(
+    <Bannercon>
+      <BannerSlider>
+       
+      </BannerSlider>
+    </Bannercon>
+  );
+};
+
+export default Banner;  
+
+const Bannercon = styled.div`
+  width: 100%;
+  height:43.74rem;
+`;
+
+const BannerSlider = styled.ul`
+  width: 100%;
+  height:100%;
+`
