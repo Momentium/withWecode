82eb1b1d892c2e063c0ee83d81421fd512206c05
mina <yegrinaaa@gmail.com>
2021-01-12 17:56:15 +0900

    0112|Mat|slider

--- front-end/offertunity-app/src/pages/banner/Banner.tsx
+++ front-end/offertunity-app/src/pages/banner/Banner.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const Banner = () => {
+
+  const [moveX,setMoveX] = useState(0);
+  const activeSlider =() =>{
+    setMoveX(moveX - 100);
+  }
+
+  
+
   return(
     <Bannercon>
-      <BannerSlider>
-       
+      <BannerSlider onClick={activeSlider} style={{ transform: `translateX(${moveX}%)` }}>
+       <li >
+         <img src="/images/greenbanner.jpg" alt=""/>
+       </li>
+       <li >
+         <img src="/images/yellowbanner.jpg" alt=""/>
+       </li>
       </BannerSlider>
     </Bannercon>
   );
@@ -19,6 +32,20 @@ const Bannercon = styled.div`
 `;
 
 const BannerSlider = styled.ul`
+  display:flex;
+  margin:0;
+  padding:0;
   width: 100%;
   height:100%;
+  transition: 0.4s;
+  li{
+    min-width:100%;
+    height:100%;
+    list-style:none;
+    img{
+      
+      width:100%;
+      height:100%;
+    }
+  }
 `
