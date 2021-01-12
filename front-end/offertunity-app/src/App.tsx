7093a3cb728c03f5884912c6d88c0b93b73c8453
SinJ <johankim27@gmail.com>
2021-01-12 12:32:12 +0900

    0112 | Sinbad | mainpage layout 브랜치 생성

--- front-end/offertunity-app/src/App.tsx
+++ front-end/offertunity-app/src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Main from './pages/main/Main';
 
 const App = () => {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      {/* Header 들어갈 자리 */}
+
+      {/* Route 들어갈 자리 */} 
+      <Route exact path="/" component={Main} />
+
+      {/* Footer 들어갈 자리 */}
+    </BrowserRouter>
   );
 }
 
---
+++ front-end/offertunity-app/src/pages/main/Main.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+const Main = () => {
+  return (
+    <StMainCont>
+    </StMainCont>
+  )
+}
+export default Main;
+
+const StMainCont = styled.div`
+  
+`;
