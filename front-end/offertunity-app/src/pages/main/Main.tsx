6b145e6c2082a781f9cf82df7d67b2199388efc7
SinJ <johankim27@gmail.com>
2021-01-12 17:06:47 +0900

    0112 | Sinbad | Main.tsx 생성

--- front-end/offertunity-app/src/pages/main/Main.tsx
+++ front-end/offertunity-app/src/pages/main/Main.tsx
@@ -4,7 +4,15 @@ import styled from 'styled-components';
 const Main = () => {
   return (
     <StMainCont>
-      
+      <div className="banner-cont"></div>
+      <div className="projects-cont">
+        <div className="progress"></div>
+        <div className="new"></div>
+      </div>
+      <div className="projects-banner"></div>
+      <div className="invest-cont"></div>
+      <div className="startup-cont"></div>
+      <div className="newsletter-cont"></div>
     </StMainCont>
   )
 }
