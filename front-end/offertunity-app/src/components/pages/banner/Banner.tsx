be69a2d34713fbd0f93dba18df08417f4cbc2322
byulyi kim <byulyikeem@gmail.com>
2021-01-14 11:25:15 +0900

    [0114] | [Pat] | [컨플릭트 해결]

--- front-end/offertunity-app/src/components/pages/banner/Banner.tsx
+++ front-end/offertunity-app/src/components/pages/banner/Banner.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
@@ -59,19 +58,3 @@ const BannerSlider = styled.ul`
     }
   }
 `;
-=======
-import React from 'react';
-import BannerPresent from '../banner/bannerUI';
-
-
-
-const Banner = () => {
-  return (
-    <div>
-      <BannerPresent img={""} script={""} title={""} />
-    </div>
-  )
-};
-
-export default Banner;
->>>>>>> 78eb8fa7c2c81a0194eebbf3937898dd636d6cfa
