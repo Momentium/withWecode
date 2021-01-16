56befb1d81f3a530c0f1580a0914bc4d1dfc7f0d
byulyi kim <byulyikeem@gmail.com>
2021-01-16 20:59:34 +0900

    0116 | Pat | 메인 필터 기능 구현 리팩토링 필요

--- front-end/offertunity-app/src/components/pages/selection/Selection.tsx
+++ front-end/offertunity-app/src/components/pages/selection/Selection.tsx
@@ -3,10 +3,11 @@ import styled from "styled-components";
 import Title from "./Title";
 import Tabs from "./Tabs";
 import Cards from "./Cards";
+import { Filter } from "@material-ui/icons";
 
 const Selection = () => {
   const [companyData, setCompanyData] = useState<any[]>([]);
-  const [filter, setFilter] = useState<String | null>("플랫폼");
+  const [filter, setFilter] = useState<String | any>("플랫폼");
 
   const handleClickTab = (event: React.MouseEvent<HTMLButtonElement>) => {
     const currText = (event.target as HTMLButtonElement).textContent;
@@ -14,30 +15,31 @@ const Selection = () => {
   };
 
   useEffect(() => {
+    companyData.length === 0 ? getCompanyData() : filterCompanyData();
+    filterCompanyData();
+  }, [filter]);
+
+  const getCompanyData = () => {
     fetch("/data/selection.json")
       .then((res) => res.json())
       .then((res) => setCompanyData(res.platform));
-  }, []);
+  };
 
-  useEffect(() => {
-    const filtered = companyData.map((data) => ({
+  const filterCompanyData = () => {
+    const filtered = companyData.map((data: any) => ({
       ...data,
       filtered: data.label.includes(filter),
     }));
     setCompanyData(filtered);
-  }, [filter]);
-
+  };
   return (
     <Selections>
       <Title title={"스타트업 셀렉션"} />
       <Tabs handleClickTab={handleClickTab} />
       <CardContainer>
-        {companyData.map((companyCard: any, idx: number) =>
-          companyCard.filtered === true ? (
-            <Cards data={companyCard} key={idx} />
-          ) : (
-            ""
-          )
+        {companyData.map(
+          (companyCard: any, idx: number) =>
+            companyCard.filtered && <Cards data={companyCard} key={idx} />
         )}
       </CardContainer>
     </Selections>
