34b440cbe87d0c57c40b3a7af57be4cc68409df2
byulyi kim <byulyikeem@gmail.com>
2021-01-18 11:09:46 +0900

    0118 | Pat | filter function

--- front-end/offertunity-app/src/components/pages/selection/Selection.tsx
+++ front-end/offertunity-app/src/components/pages/selection/Selection.tsx
@@ -4,10 +4,12 @@ import Title from "./Title";
 import Tabs from "./Tabs";
 import Cards from "./Cards";
 import { Filter } from "@material-ui/icons";
+import { JsxElement } from "typescript";
 
 const Selection = () => {
   const [companyData, setCompanyData] = useState<any[]>([]);
   const [filter, setFilter] = useState<String | any>("플랫폼");
+  const [filtered, setFiltered] = useState<any[]>([]);
 
   const handleClickTab = (event: React.MouseEvent<HTMLButtonElement>) => {
     const currText = (event.target as HTMLButtonElement).textContent;
@@ -15,15 +17,30 @@ const Selection = () => {
   };
 
   useEffect(() => {
-    companyData.length === 0 ? getCompanyData() : filterCompanyData();
-    filterCompanyData();
-  }, [filter]);
-
-  const getCompanyData = () => {
     fetch("/data/selection.json")
       .then((res) => res.json())
-      .then((res) => setCompanyData(res.platform));
-  };
+      .then((res) => setCompanyData(res.platform))
+      .then((res) => {
+        setFiltered(
+          companyData.map((companyCard: any, idx: number) => {
+            return companyCard.label.includes(filter) ? (
+              <Cards data={companyCard} key={idx} />
+            ) : null;
+          })
+        );
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log(companyData);
+    setFiltered(
+      companyData.map((companyCard: any, idx: number) => {
+        return companyCard.label.includes(filter) ? (
+          <Cards data={companyCard} key={idx} />
+        ) : null;
+      })
+    );
+  }, [filter]);
 
   const filterCompanyData = () => {
     const filtered = companyData.map((data: any) => ({
@@ -36,12 +53,7 @@ const Selection = () => {
     <Selections>
       <Title title={"스타트업 셀렉션"} />
       <Tabs handleClickTab={handleClickTab} />
-      <CardContainer>
-        {companyData.map(
-          (companyCard: any, idx: number) =>
-            companyCard.filtered && <Cards data={companyCard} key={idx} />
-        )}
-      </CardContainer>
+      <CardContainer>{filtered}</CardContainer>
     </Selections>
   );
 };
