012d2e3b7f8bbf5780b6ff769a5455d2b79cac88
mina <yegrinaaa@gmail.com>
2021-01-27 15:35:45 +0900

    0127 | Mat | Router state 값에 담아서 정리

--- front-end/offertunity-app/src/App.tsx
+++ front-end/offertunity-app/src/App.tsx
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route, withRouter, RouteComponentProps } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  withRouter,
+  RouteComponentProps,
+} from "react-router-dom";
 import styled, { ThemeProvider } from "styled-components";
 import Header from "./components/common/header/Header";
 import Banner from "./components/common/banner/Banner";
@@ -8,45 +13,43 @@ import ProjectPage from "./components/pages/project/ProjectPage";
 import Newsletter from "./components/common/newsletter/Newsletter";
 import Footer from "./components/common/footer/Footer";
 import theme from "./components/styles/theme";
-import Auth from "./components/pages/Auth/Auth"
+import Auth from "./components/pages/Auth/Auth";
 import StartupList from "./components/pages/startupList/StartupList";
 
-
-const App:React.FC<RouteComponentProps<any>> = ({location }) => {
+const App: React.FC<RouteComponentProps<any>> = ({ location }) => {
   const [HH, setHH] = useState<number | undefined>(60);
-  useEffect(() => {
-    console.log(location.pathname)
-  }, []);
+  useEffect(() => {}, []);
+  const [navHidden, setNavHidden] = useState(true);
+  const [visibleBanner, setVisibleBanner] = useState(true);
+  const [visibleNewslatter, setVisibleNewslatter] = useState(true);
+  const [visiblefooter, setVisibleFooter] = useState(true);
 
-    const Reject = location.pathname.includes("/Auth")
+  useEffect(() => {
+    window.location.pathname === "/details" && setVisibleBanner(false);
+    window.location.pathname.includes("/Auth") && setVisibleBanner(false);
+    window.location.pathname.includes("/Auth") && setNavHidden(false);
+    window.location.pathname.includes("/Auth") && setVisibleNewslatter(false);
+    window.location.pathname.includes("/Auth") && setVisibleFooter(false);
+  });
 
   return (
     <ThemeProvider theme={theme}>
-      
-        {/* Header 들어갈 자리 */}
-        {
-         !Reject  &&
-          <>
-            <Header />
-            <Banner />
-          </>
-        }
-        <Route path="/Auth/:name" component={Auth} />
-        <StAppCont headerHeight={HH}>
-          {/* Route 들어갈 자리 */}
-          <Route exact path="/" component={Main} />
-          <Route path="/project" component={ProjectPage} />
-          <Route path="/list" component={StartupList} />
-        </StAppCont>
-         {/* Footer 들어갈 자리 */}
-        {
-           !Reject  &&
-          <>
-            <Newsletter />
-            <Footer />
-          </>
-        }
-        
+      {/* Header 들어갈 자리 */}
+
+      {navHidden && <Header />}
+      {visibleBanner && <Banner />}
+
+      <Route path="/Auth/:name" component={Auth} />
+      <StAppCont headerHeight={HH}>
+        {/* Route 들어갈 자리 */}
+        <Route exact path="/" component={Main} />
+        <Route path="/project" component={ProjectPage} />
+        <Route path="/list" component={StartupList} />
+      </StAppCont>
+      {/* Footer 들어갈 자리 */}
+
+      {visibleNewslatter && <Newsletter />}
+      {visiblefooter && <Footer />}
     </ThemeProvider>
   );
 };
