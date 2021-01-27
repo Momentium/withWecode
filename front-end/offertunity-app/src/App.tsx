9124b9f1e61006a7c17a0a1c4c0f1e4e71cc1731
mina <yegrinaaa@gmail.com>
2021-01-27 15:24:13 +0900

    0127 | Mat | Mypage 시작

--- front-end/offertunity-app/src/App.tsx
+++ front-end/offertunity-app/src/App.tsx
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route, withRouter, RouteComponentProps } from "react-router-dom";
+import {
+  BrowserRouter,
+  Route,
+  withRouter,
+  RouteComponentProps,
+} from "react-router-dom";
 import styled, { ThemeProvider } from "styled-components";
 import Header from "./components/common/header/Header";
 import Banner from "./components/common/banner/Banner";
@@ -8,45 +13,43 @@ import ProjectPage from "./components/pages/project/ProjectPage";
 import Newsletter from "./components/common/newsletter/Newsletter";
 import Footer from "./components/common/footer/Footer";
 import theme from "./components/styles/theme";
-import Auth from "./components/pages/Auth/Auth"
+import Auth from "./components/pages/Auth/Auth";
 import StartupList from "./components/pages/startupList/StartupList";
+import MypageStartup from "./components/pages/mypage/MypageStartup";
 
-
-const App:React.FC<RouteComponentProps<any>> = ({location }) => {
+const App: React.FC<RouteComponentProps<any>> = ({ location }) => {
   const [HH, setHH] = useState<number | undefined>(60);
   useEffect(() => {
-    console.log(location.pathname)
+    console.log(location.pathname);
   }, []);
 
-    const Reject = location.pathname.includes("/Auth")
+  const Reject = location.pathname.includes("/Auth");
+  const [visible, setVisible] = useState("");
 
   return (
     <ThemeProvider theme={theme}>
-      
-        {/* Header 들어갈 자리 */}
-        {
-         !Reject  &&
-          <>
-            <Header />
-            <Banner />
-          </>
-        }
-        <Route path="/Auth/:name" component={Auth} />
-        <StAppCont headerHeight={HH}>
-          {/* Route 들어갈 자리 */}
-          <Route exact path="/" component={Main} />
-          <Route path="/project" component={ProjectPage} />
-          <Route path="/list" component={StartupList} />
-        </StAppCont>
-         {/* Footer 들어갈 자리 */}
-        {
-           !Reject  &&
-          <>
-            <Newsletter />
-            <Footer />
-          </>
-        }
-        
+      {/* Header 들어갈 자리 */}
+      {!Reject && (
+        <>
+          <Header />
+          <Banner />
+        </>
+      )}
+      <Route path="/Auth/:name" component={Auth} />
+      <StAppCont headerHeight={HH}>
+        {/* Route 들어갈 자리 */}
+        <Route exact path="/" component={Main} />
+        <Route path="/project" component={ProjectPage} />
+        <Route path="/list" component={StartupList} />
+        <Route path="/MypageStartup" component={MypageStartup} />
+      </StAppCont>
+      {/* Footer 들어갈 자리 */}
+      {!Reject && (
+        <>
+          <Newsletter />
+          <Footer />
+        </>
+      )}
     </ThemeProvider>
   );
 };
---
+++ front-end/offertunity-app/src/components/pages/mypage/MypageStartup.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Profile from "./components/Profile";
+
+const MypageStartup = () => {
+  return (
+    <div>
+      <Profile />
+    </div>
+  );
+};
+
+export default MypageStartup;
