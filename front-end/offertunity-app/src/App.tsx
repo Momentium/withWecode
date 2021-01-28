c0c6a55478bf2a9d96953ebb9fa80119ef377dfd
SinJ <johankim27@gmail.com>
2021-01-28 18:22:04 +0900

    0128 | Sinbad | refactoring

--- front-end/offertunity-app/src/App.tsx
+++ front-end/offertunity-app/src/App.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Route, withRouter, RouteComponentProps } from "react-router-dom";
+import { Switch, Route, withRouter, RouteComponentProps } from "react-router-dom";
 import styled, { ThemeProvider } from "styled-components";
 import Header from "./components/common/header/Header";
 import Banner from "./components/common/banner/Banner";
 import Main from "./components/pages/main/Main";
 import ProjectPage from "./components/pages/project/ProjectPage";
+import ProjectDetail from "./components/pages/project/detail/ProjectDetailPage";
 import Newsletter from "./components/common/newsletter/Newsletter";
 import Footer from "./components/common/footer/Footer";
 import theme from "./components/styles/theme";
@@ -26,11 +27,17 @@ const App:React.FC<RouteComponentProps<any>> = ({ location }) => {
         {/* Header 들어갈 자리 */}
         <Header ref={headerRef}/>
         <StAppCont headMargin={headMargin}>
-          { !location.pathname.includes("auth") && <Banner /> }
+          { !location.pathname.includes("auth") && 
+            !location.pathname.includes("detail") && 
+            <Banner /> 
+          }
 
           {/* Route 들어갈 자리 */}
           <Route exact path="/" component={Main} />
-          <Route path="/project" component={ProjectPage} />
+          <Switch>
+              <Route path="/project/detail/:id" component={ProjectDetail}/>
+              <Route path="/project" component={ProjectPage}/>
+          </Switch>
           <Route path="/startup" component={StartupList} />
           <Route path="/auth/:name" component={Auth} />
           <Route path="/details" component={StartupDetails} />
