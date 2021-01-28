94bce9e7868503a4f14b08f9b3f4603c3f45b095
Peter Oh <petersoh414@gmail.com>
2021-01-28 18:05:10 +0900

    0128| Peter | 이메일인증

--- back-end/offertunity/controllers/CompanyController.js
+++ back-end/offertunity/controllers/CompanyController.js
@@ -1,23 +1,24 @@
 const { CompanyService } = require('../services')
 const dayjs = require('dayjs')
-const { typeChecker, lengthChecker } = require('../utils')
+const { typeChecker, lengthChecker, dateForm } = require('../utils')
 const { errorWrapper, errorGenerator } = require('../errors');
 
 
-const startupInfoTempSave = errorWrapper(async(req, res) => {
-    if (!req.foundUser.type_id === 2) errorGenerator({ statusCode: 400, message: 'this user is not startup user' })
+const startupInfoTempSave = errorWrapper(async(req, res, next) => {
+    if (req.foundUser.type_id === 2) errorGenerator({ statusCode: 400, message: 'this user is not startup user' })
 
     const { name, rep, establishedDate, sectorId, coreTechnologyId, homepage, description, itemDescription, investmentSeriesId, wishInvestmentSeriesIds, investmentFundId, teamIntro, memberCount, memberInfoNames, memberDeleteIds, memberInfoPositions, companyNewsURLs, companyNewsDeleteIds, logoImgURL, startupImagesDeleteIds, investedDates, investedInstitutions, investedFunds, investedValues, investedSeries, investedDeleteIds, thumbnailURL } = req.body
     const { logoImg, startupImages, memberImages, thumbnail } = req.files
 
-    const company_fields = {
+    const companyFields = {
         name,
         logo_img: logoImg ? logoImg[0].location : logoImgURL ? logoImgURL : null,
-        established_date: establishedDate,
+        established_date: await dateForm(establishedDate),
         homepage,
         description,
         team_intro: teamIntro,
-        member_count: memberCount,
+        member_count: Number(memberCount),
+        company_types: { connect: { id: 1 } }
     }
     const startup_connect = {
         sectors: { connect: { id: Number(sectorId) } },
@@ -31,21 +32,21 @@ const startupInfoTempSave = errorWrapper(async(req, res) => {
         thumbnail: thumbnail ? thumbnail[0].location : thumbnailURL ? thumbnailURL : null,
     }
 
-    Object.keys(company_fields).forEach(key => company_fields[key] === undefined ? company_fields[key] = null : {});
+    Object.keys(companyFields).forEach(key => companyFields[key] === undefined ? companyFields[key] = null : {});
     Object.keys(startup_connect).forEach(key => isNaN(startup_connect[key].connect.id) ? startup_connect[key] = undefined : {});
     Object.keys(startup_field).forEach(key => startup_field[key] === undefined ? startup_field[key] = null : {});
 
-    const startup_fields = {...startup_connect, ...startup_field }
+    const startupFields = {...startup_connect, ...startup_field }
 
     let companyInfo
     let startupInfo
 
     if (!req.foundUser.company_id) {
-        companyInfo = await CompanyService.createCompany(req.foundUser.id, company_fields);
-        startupInfo = await CompanyService.createStartup(companyInfo.id, startup_fields);
+        companyInfo = await CompanyService.createCompany(req.foundUser.id, companyFields);
+        startupInfo = await CompanyService.createCompanyDetail(companyInfo.id, 'startups', startupFields);
     } else {
-        companyInfo = await CompanyService.updateCompany(req.foundUser.company_id, company_fields);
-        startupInfo = await CompanyService.updateStartup(companyInfo.id, startup_fields);
+        companyInfo = await CompanyService.updateCompany(req.foundUser.company_id, companyFields);
+        startupInfo = await CompanyService.updateStartup(companyInfo.id, startupFields);
     };
 
     // startup 이미지 추가
@@ -143,10 +144,9 @@ const startupInfoTempSave = errorWrapper(async(req, res) => {
     // 투자 이력 추가
     if (investedDates && investedInstitutions && investedFunds && investedValues && investedSeries) {
         if (typeChecker(investedDates, investedInstitutions, investedFunds, investedValues, investedSeries) === 'string') {
-            const datemodified = await dayjs(investedDates).toDate()
             await CompanyService.createRelatedInfo('invested_from', data = {
                 startups: { connect: { id: startupInfo.id } },
-                date: datemodified,
+                date: await dateForm(investedDates),
                 invested_institution: investedInstitutions,
                 investment_funds: { connect: { id: Number(investedFunds) } },
                 corporate_value: Number(investedValues),
@@ -157,7 +157,7 @@ const startupInfoTempSave = errorWrapper(async(req, res) => {
                 const datemodified = await dayjs(investedDates[len]).toDate()
                 await CompanyService.createRelatedInfo('invested_from', data = {
                     startups: { connect: { id: startupInfo.id } },
-                    date: datemodified,
+                    date: await dateForm(investedDates),
                     invested_institution: investedInstitutions[len],
                     investment_funds: { connect: { id: Number(investedFunds[len]) } },
                     corporate_value: Number(investedValues[len]),
@@ -251,22 +251,26 @@ const startupInfoTempSave = errorWrapper(async(req, res) => {
     };
 
     // save 기능 연결
-    if (req.save) {
+    if (req.companySave) {
         next()
+    } else {
+        await res.status(201).json({
+            message: 'startup info temporary saved'
+        })
     }
-
-    await res.status(201).json({
-        message: 'startup info temporary saved'
-    })
 });
 
 const startupInfoSave = errorWrapper(async(req, res) => {
-    if (req.foundUser.type_id === 2) errorGenerator({ statusCode: 400, message: 'this user is not startup user' })
-    const company = CompanyService.readCompany(req.foundUser.company_id)
-    const startup = CompanyService.readStartup(req.foundUser.company_id)
+    const companyId = req.foundUser.company_id
+
+    const company = CompanyService.readCompany(companyId)
+    const startup = CompanyService.readCompanyDetail('startups', companyId)
+
     if (company === null) errorGenerator({ statusCode: 400, message: 'unidentified company data' })
     if (startup === null) errorGenerator({ statusCode: 400, message: 'unidentified startup data' })
     if (company.name || company.description || startup.rep || startup.sector_id || startup.core_technology_id || startup.item_description) errorGenerator({ statusCode: 400, message: 'unfilled required infos' })
+
+    CompanyService.saveInfo(companyId)
 
     await res.status(201).json({
         message: 'startup info saved'
@@ -315,19 +319,218 @@ const startupProjectSubmitSave = errorWrapper(async(req, res) => {
     };
 
     await res.status(201).json({
-        message: 'startup info saved'
+        message: 'startup project info saved'
     })
 })
 
-const partnerInfoTempSave = errorWrapper(async(req, res) => {
+const partnerInfoTempSave = errorWrapper(async(req, res, next) => {
     if (req.foundUser.type_id === 1) errorGenerator({ statusCode: 400, message: 'this user is not partner user' })
-    const { name, rep, establishedDate, sectorId, coreTechnologyId, homepage, description, itemDescription, investmentSeriesId, wishInvestmentSeriesIds, investmentFundId, teamIntro, memberCount, memberInfoNames, memberDeleteIds, memberInfoPositions, companyNewsURLs, companyNewsDeleteIds, logoImgURL, startupImagesDeleteIds, investedDates, investedInstitutions, investedFunds, investedValues, investedSeries, investedDeleteIds } = req.body
-    const { logoImg, startupImages, memberImages } = req.files
+    const { name, establishedDate, investedCounts, totalInvestedId, interedtedTechnologyId, homepage, description, investedDates, investedStartups, investedFunds, investedValues, investedSeries, teamIntro, memberCount, memberInfoNames, memberInfoPositions, companyNewsURLs, portfolioImagesDeleteIds, investedDeleteIds, memberDeleteIds, companyNewsDeleteIds } = req.body
+    const { logoImg, portfolioImages, memberImages } = req.files
+
+    const companyFields = {
+        name,
+        logo_img: logoImg ? logoImg[0].location : logoImgURL ? logoImgURL : null,
+        established_date: await dateForm(establishedDate),
+        homepage,
+        description,
+        team_intro: teamIntro,
+        member_count: Number(memberCount),
+    }
+    const partner_connect = {
+        investment_funds: { connect: { id: Number(totalInvestedId) } },
+        technologies: { connect: { id: Number(interedtedTechnologyId) } }
+    }
+    const partner_field = {
+        invested_counts: investedCounts,
+    }
+
+    Object.keys(companyFields).forEach(key => companyFields[key] === undefined ? companyFields[key] = null : {});
+    Object.keys(partner_connect).forEach(key => isNaN(partner_connect[key].connect.id) ? partner_connect[key] = undefined : {});
+    Object.keys(partner_field).forEach(key => partner_field[key] === undefined ? partner_field[key] = null : {});
+
+    const partnerFields = {...partner_connect, ...partner_field }
+
+    let companyInfo
+    let partnerInfo
+
+    if (!req.foundUser.company_id) {
+        companyInfo = await CompanyService.createCompany(req.foundUser.id, companyFields);
+        partnerInfo = await CompanyService.createCompanyDetail(companyInfo.id, 'partners', partnerFields);
+    } else {
+        companyInfo = await CompanyService.updateCompany(req.foundUser.company_id, companyFields);
+        partnerInfo = await CompanyService.updatePartner(companyInfo.id, partnerFields);
+    };
+
+    // portfolio 이미지 추가
+    if (portfolioImages) {
+        for (len = 0; len < portfolioImages.length; len++) {
+            await CompanyService.createRelatedInfo('investment_portfolio',
+                data = {
+                    partners: { connect: { id: partnerInfo.id } },
+                    img_url: portfolioImages[len].location
+                }
+            )
+        }
+    }
+
+    // portfolio 이미지 삭제
+    if (portfolioImagesDeleteIds) {
+        if (typeof portfolioImagesDeleteIds === 'string') {
+            if (await CompanyService.checkExistence('investment_portfolio', data = { id: Number(portfolioImagesDeleteIds) })) {
+                await CompanyService.deleteRelatedInfo('investment_portfolio', Number(portfolioImagesDeleteIds))
+            };
+        } else {
+            for (len = 0; len < portfolioImagesDeleteIds.length; len++) {
+                if (await CompanyService.checkExistence('investment_portfolio', data = { id: Number(portfolioImagesDeleteIds[len]) })) {
+                    await CompanyService.deleteRelatedInfo('investment_portfolio', Number(portfolioImagesDeleteIds[len]))
+                }
+            };
+        }
+    }
+
+    // 투자 이력 추가
+    if (investedDates && investedStartups && investedFunds && investedValues && investedSeries) {
+        if (typeChecker(investedDates, investedStartups, investedFunds, investedValues, investedSeries) === 'string') {
+            await CompanyService.createRelatedInfo('invested_from', data = {
+                partners: { connect: { id: partnerInfo.id } },
+                date: await dateForm(investedDates),
+                invested_startup: investedStartups,
+                investment_funds: { connect: { id: Number(investedFunds) } },
+                corporate_value: Number(investedValues),
+                investment_series: { connect: { id: Number(investedSeries) } }
+            })
+        } else if (lengthChecker(investedDates, investedStartups, investedFunds, investedValues, investedSeries)) {
+            for (len = 0; len < investedDates.length; len++) {
+                const datemodified = await dayjs(investedDates[len]).toDate()
+                await CompanyService.createRelatedInfo('invested_from', data = {
+                    partners: { connect: { id: partnerInfo.id } },
+                    date: await dateForm(investedDates),
+                    invested_startup: investedStartups[len],
+                    investment_funds: { connect: { id: Number(investedFunds[len]) } },
+                    corporate_value: Number(investedValues[len]),
+                    investment_series: { connect: { id: Number(investedSeries[len]) } }
+                })
+            }
+        }
+    }
+
+    // 투자 이력 삭제
+    if (investedDeleteIds) {
+        if (typeof investedDeleteIds === 'string') {
+            if (await CompanyService.checkExistence('invested_to', data = { id: Number(investedDeleteIds) })) {
+                await CompanyService.deleteRelatedInfo('invested_to', Number(investedDeleteIds))
+            };
+        } else {
+            for (len = 0; len < investedDeleteIds.length; len++) {
+                if (await CompanyService.checkExistence('invested_to', data = { id: Number(investedDeleteIds[len]) })) {
+                    await CompanyService.deleteRelatedInfo('invested_to', Number(investedDeleteIds[len]))
+                }
+            };
+        }
+    };
+
+
+    // Team Memebr 추가
+    if (memberInfoNames && memberInfoPositions && memberImages) {
+        if (typeChecker(memberInfoNames, memberInfoPositions) === 'string' && memberImages.length === 1) {
+            await CompanyService.createRelatedInfo('company_members', data = {
+                companies: { connect: { id: companyInfo.id } },
+                name: memberInfoNames,
+                img: memberImages[0].location,
+                position: memberInfoPositions
+            })
+        } else if (lengthChecker(memberInfoNames, memberInfoPositions, memberImages)) {
+            for (len = 0; len < memberInfoNames.length; len++) {
+                await CompanyService.createRelatedInfo('company_members', data = {
+                    companies: { connect: { id: companyInfo.id } },
+                    name: memberInfoNames[len],
+                    img: memberImages[len].location,
+                    position: memberInfoPositions[len]
+                })
+            }
+        }
+    }
+
+    // Team Memebr 삭제
+    if (memberDeleteIds) {
+        if (typeof memberDeleteIds === 'string') {
+            if (await CompanyService.checkExistence('company_members', data = { id: Number(memberDeleteIds) })) {
+                await CompanyService.deleteRelatedInfo('company_members', Number(memberDeleteIds))
+            };
+        } else {
+            for (len = 0; len < memberDeleteIds.length; len++) {
+                if (await CompanyService.checkExistence('company_members', data = { id: Number(memberDeleteIds[len]) })) {
+                    await CompanyService.deleteRelatedInfo('company_members', Number(memberDeleteIds[len]))
+                }
+            };
+        }
+    };
+
+
+    // News URL 추가
+    if (companyNewsURLs) {
+        if (typeof companyNewsURLs === 'string') {
+            await CompanyService.createRelatedInfo('company_news', data = {
+                companies: { connect: { id: companyInfo.id } },
+                URL: companyNewsURLs
+            })
+        } else {
+            for (len = 0; len < companyNewsURLs.length; len++) {
+                await CompanyService.createRelatedInfo('company_news', data = {
+                    companies: { connect: { id: companyInfo.id } },
+                    URL: companyNewsURLs[len]
+                })
+            }
+        }
+    }
+
+    // News URL 삭제
+    if (companyNewsDeleteIds) {
+        if (typeof companyNewsDeleteIds === 'string') {
+            if (await CompanyService.checkExistence('company_news', data = { id: Number(companyNewsDeleteIds) })) {
+                await CompanyService.deleteRelatedInfo('company_news', Number(companyNewsDeleteIds))
+            };
+        } else {
+            for (len = 0; len < companyNewsDeleteIds.length; len++) {
+                if (await CompanyService.checkExistence('company_news', data = { id: Number(companyNewsDeleteIds[len]) })) {
+                    await CompanyService.deleteRelatedInfo('company_news', Number(companyNewsDeleteIds[len]))
+                }
+            };
+        }
+    };
+
+    // save 기능 연결
+    if (req.companySave) {
+        next()
+    } else {
+        await res.status(201).json({
+            message: 'partner info temporary saved'
+        })
+    }
+})
+
+const partnerInfoSave = errorWrapper(async(req, res) => {
+    const companyId = req.foundUser.company_id
+
+    const company = CompanyService.readCompany(companyId)
+    const partner = CompanyService.readStartup('partners', companyId)
+
+    if (company === null) errorGenerator({ statusCode: 400, message: 'unidentified company data' })
+    if (partner === null) errorGenerator({ statusCode: 400, message: 'unidentified partner data' })
+    if (company.name || company.description || company.logo_img || partner.invested_counts || partner.interst_technology_id) errorGenerator({ statusCode: 400, message: 'unfilled required infos' })
+
+    CompanyService.saveInfo(companyId)
+
+    await res.status(201).json({
+        message: 'partner info saved'
+    })
 })
 
 module.exports = {
     startupInfoTempSave,
     startupInfoSave,
+    startupProjectSubmitSave,
     partnerInfoTempSave,
-    startupProjectSubmitSave
+    partnerInfoSave
 }
