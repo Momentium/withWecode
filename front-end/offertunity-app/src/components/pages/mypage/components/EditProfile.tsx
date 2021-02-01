d41661b0a6546f0fbb0aa1309e1a090d611f6f09
mina <yegrinaaa@gmail.com>
2021-02-01 17:05:53 +0900

    0201 | Mat | 마이페이지 _ 프로필정보수정 레이아웃 완료

--- front-end/offertunity-app/src/components/pages/mypage/components/EditProfile.tsx
+++ front-end/offertunity-app/src/components/pages/mypage/components/EditProfile.tsx
@@ -15,14 +15,13 @@ const EditProfile = () => {
     reader.readAsDataURL(file);
   };
 
-  const handleButton = (event: any) => {};
-
   return (
     <Img>
       <span>
         <img src={previewURL} alt="프로필사진" />
       </span>
       <label>
+        프로필 사진등록
         <input
           id="upload"
           type="file"
@@ -30,8 +29,10 @@ const EditProfile = () => {
           onChange={handleImg}
         />
       </label>
-
-      <button onClick={handleButton}>프로필 사진 등록</button>
+      <button>
+        <p>회원탈퇴</p>
+        <i className="fas fa-chevron-right" />
+      </button>
     </Img>
   );
 };
@@ -39,6 +40,7 @@ const EditProfile = () => {
 export default EditProfile;
 
 const Img = styled.div`
+  position: relative;
   width: 20%;
   margin: 0 10.5rem 0 10.06rem;
   text-align: center;
@@ -54,6 +56,35 @@ const Img = styled.div`
     }
   }
   input {
+    position: absolute;
+    top: 14rem;
+    left: 2rem;
     margin-top: 2.5rem;
+    z-index: -8;
+  }
+  label {
+    position: absolute;
+    top: 14rem;
+    left: 2rem;
+    width: 11.5rem;
+    height: 2.5rem;
+    line-height: 2.5rem;
+    border: 1px solid #1a2536;
+    border-radius: 24px;
+    font-weight: bold;
+    cursor: pointer;
+  }
+  button {
+    margin-top: 7rem;
+    font-size: 0.8rem;
+    color: #898989;
+    cursor: pointer;
+    p {
+      display: inline-block;
+      border-bottom: 1px solid #898989;
+    }
+    i {
+      margin-left: 0.5rem;
+    }
   }
 `;
