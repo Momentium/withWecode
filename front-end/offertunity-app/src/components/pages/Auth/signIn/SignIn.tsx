28104ca9646f1f5bc1b51cd7485e854ea5e9a4d6
mina <yegrinaaa@gmail.com>
2021-02-03 15:58:12 +0900

    0202 | Mat | 로그인시 헤더 변경 & 로그아웃기능 완료

--- front-end/offertunity-app/src/components/pages/Auth/signIn/SignIn.tsx
+++ front-end/offertunity-app/src/components/pages/Auth/signIn/SignIn.tsx
@@ -25,8 +25,24 @@ const SignIn: React.FC = () => {
       })
       .then(function (response) {
         alert("로그인 성공");
+        
+        console.log(response.data.foundUser)
+        const _resData = response.data.foundUser;
+      
+        const _userInfo:{} = {
+          id: _resData.id,
+          email: _resData.email,
+          name: _resData.name,
+          type_id: _resData.type_id,
+        }
+        
         sessionStorage.setItem("token", response.data.token);
+        sessionStorage.setItem("userInfo", JSON.stringify(_userInfo));
+        
+        console.log(JSON.parse(String(sessionStorage.getItem("userInfo"))))
         window.location.href = "/";
+       
+     
       })
       .catch(function (error) {
         alert("로그인 실패");
